38ca42e21b10065e253e88cc16ee363356f98bab
AuguRai <113252701+AuguRai@users.noreply.github.com>
2022-09-23 00:28:50 +0300

    Create MyLib.h

---
+++ MyLib.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <time.h>
+#include <limits>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::setprecision;
+using std::fixed;
+using std::sort;
+using std::vector;
